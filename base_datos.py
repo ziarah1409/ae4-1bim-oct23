95a03c7193a3623c938c8388dc32f4c690eed4e2
Ziarah <zlapolo@utpl.edu.ec>
2023-11-30 00:19:09 -0500

    Actualización del Respositorio Mongo

---
+++ base_datos.py
@@ -0,0 +1,8 @@
+"""
+"""
+from pymongo import MongoClient
+# se genera en enlace al gestor de base de
+# datos para el ejemplo se usa la base de datos
+# sqlite
+engine = MongoClient ('mongodb+srv://zapolorivera:Ziarah140@cluster0.zyjdlhv.mongodb.net/?retryWrites=true&w=majority')
+
---
+++ crear_entidades.py
@@ -0,0 +1,17 @@
+# se importa el engine
+from base_datos import engine
+
+# se crea la clase llamada Base que permite definir las clases bajo las
+# características de SQLAlchemy
+db = engine["Matriculas"]
+collection = db["RegistroEstudiantes"]
+# Se crea la una entidad llamada Autor, que hereda
+# de Base
+
+
+data_01 ={"Nombre":"Ziarah", "Apellido":"Apolo","Curso":"Tranformacion Digital de Empresas", "Universidad":"UTPL"}
+collection.insert_one(data_01)
+
+collection2 = db ["RegistroVehiculos"]
+data_02 ={ "Marca":"Toyota","Modelo":2023,"Motor":"2.9", "Color":"Gris"}
+collection2.insert_one(data_02)
